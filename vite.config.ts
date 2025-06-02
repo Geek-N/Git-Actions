e15a6a9c49e1dc309400c9f28b24eb39c88dd0dc
Nina W <nina@Ninas-Air.lan>
2025-06-01 19:18:26 -0700

    improved code and bugs

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/_tests_/setup.ts'
+  },
   server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT ?? '3000'),
+    port: 3000,
     open: true,
     proxy: {
       '/api': {
@@ -13,7 +18,6 @@ export default defineConfig({
         secure: false,
         changeOrigin: true
       }
-    },
-    allowedHosts: ['.onrender.com']
+    }
   }
 })
