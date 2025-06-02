7a7bafcf86649ad81a0df09cc3cf8acf4fe9920b
Nina W <nina@Ninas-Air.lan>
2025-06-01 19:24:06 -0700

    fixed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,19 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
-    port: parseInt(process.env.PORT ?? '3000'),
+    port: 3000,
     open: true,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
+        changeOrigin: true,
         secure: false,
-        changeOrigin: true
-      }
+      },
     },
-    allowedHosts: ['.onrender.com']
-  }
-})
+  },
+});
